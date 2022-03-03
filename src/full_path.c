291b0c596cde57927a1323c6bae8c1e904931c4f
Bill <cgx@mail.ustc.edu.cn>
2022-03-03 22:39:29 +0800

    Modify project hierarchy.

--- src/full_path.c
+++ src/full_path.c
@@ -1,6 +1,5 @@
 #include "built_in.h"
 
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
--- src/init_wd.c
+++ src/init_wd.c
@@ -11,7 +11,7 @@ char* init_wd() {
     char* buffer = (char*) calloc(20, sizeof(char));
     while (!getcwd(buffer, buffer_size)) {
         buffer_size <<= 1;
-        reallocarray(buffer, buffer_size, sizeof(char));
+        buffer = reallocarray(buffer, buffer_size, sizeof(char));
     }
     if (!strcmp(user_home_directory, buffer)) return "~";
     return buffer;
